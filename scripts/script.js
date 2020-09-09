eaf1b6720c666117646538144eb0c8c925568433
Kortni-BOO <58441596+Kortni-BOO@users.noreply.github.com>
2020-09-09 22:19:43 +0300

    1 rev

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
-
-
-
 const popup = document.querySelector('.popup')
-const popupOpenButton = document.querySelector('.js-profile__open-popup')
+const popupOpenButton = document.querySelector('.profile__button-edit')
 const popupCloseButton = document.querySelector('.popup__close')
+const formElement = document.querySelector('.popup__form')
+const nameInput = document.querySelector('.popup__input_name')
+const jobInput = document.querySelector('.popup__input_job')
+const profileName = document.querySelector('.profile__name')
+const profileJob = document.querySelector('.profile__job')
 
 
 
@@ -11,41 +13,29 @@ const popupCloseButton = document.querySelector('.popup__close')
 // const popupToggle = function() {
 //     popup.classList.toggle('popup__is-opened')
 // }
-
-const popupAdd = function() {
-    popup.classList.add('popup__is-opened')
+function popupAdd() {
+    popup.classList.add('popup__is-opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 
-const popupRemove = function() {
+function popupRemove() {
     popup.classList.remove('popup__is-opened')
 }
+
 
 popupOpenButton.addEventListener('click', popupAdd)
 popupCloseButton.addEventListener('click', popupRemove)
 
 
-
-const formElement = document.querySelector('.popup__form')
-const PopupSaveButton = document.querySelector('.popup__save')
-const nameInput = document.querySelector('.form__name')
-const jobInput = document.querySelector('.form__job')
-const profileName = document.querySelector('.profile__name')
-const profileJob = document.querySelector('.profile__job')
-
-
  function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
-    // nameInput.value;
-    // jobInput.value;
-    // profileName.textContent = "Жак-Ив Кусто";
-    // profileJob.textContent = "Исследователь океана";
+
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-
+    popupRemove()
+    
 }
 
-
-
 formElement.addEventListener('submit', formSubmitHandler);
-PopupSaveButton.addEventListener('click', popupRemove);
