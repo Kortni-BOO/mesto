d63d1f9cf26b015c39fd41c8a7b8301cdb0469d1
Kortni-BOO <58441596+Kortni-BOO@users.noreply.github.com>
2020-09-11 19:33:46 +0300

    final

--- scripts/script.js
+++ scripts/script.js
@@ -2,40 +2,31 @@ const popup = document.querySelector('.popup')
 const popupOpenButton = document.querySelector('.profile__button-edit')
 const popupCloseButton = document.querySelector('.popup__close')
 const formElement = document.querySelector('.popup__form')
-const nameInput = document.querySelector('.popup__input_name')
-const jobInput = document.querySelector('.popup__input_job')
+const nameInput = document.querySelector('.popup__input-name-author')
+const jobInput = document.querySelector('.popup__input-job-author')
 const profileName = document.querySelector('.profile__name')
 const profileJob = document.querySelector('.profile__job')
 
 
-
-
-// const popupToggle = function() {
-//     popup.classList.toggle('popup__is-opened')
-// }
 function popupAdd() {
-    popup.classList.add('popup__is-opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
+    popup.classList.add('popup__is-opened');
 }
 
 function popupRemove() {
     popup.classList.remove('popup__is-opened')
 }
-
-
-popupOpenButton.addEventListener('click', popupAdd)
-popupCloseButton.addEventListener('click', popupRemove)
-
-
- function formSubmitHandler (evt) {
+function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
 
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popupRemove()
+    popupRemove();
     
 }
 
+popupOpenButton.addEventListener('click', popupAdd);
+popupCloseButton.addEventListener('click', popupRemove);
 formElement.addEventListener('submit', formSubmitHandler);
